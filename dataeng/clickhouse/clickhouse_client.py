2259e02c9da417949d8d7646208b6d9874e22027
John Grubb <git@grubb.johnnygrubb.com>
2023-12-19 13:13:51 -0500

    Updating clickhouse client for PSH

--- dataeng/clickhouse/clickhouse_client.py
+++ dataeng/clickhouse/clickhouse_client.py
@@ -4,15 +4,26 @@ from platformshconfig import Config as PlatformshConfig
 
 
 def create_client():
-    psh_config = PlatformshConfig()
-    psh_creds = psh_config.credentials("clickhouse_db")
+    try:
+        psh_config = PlatformshConfig()
+        psh_creds = psh_config.credentials("clickhouse_db")
+    except Exception as e:
+        psh_creds = None
 
-    credentials = dict(
-        host=os.getenv("CLICKHOUSE_HOST", "localhost"),
-        username=os.getenv("CLICKHOUSE_USERNAME", "default"),
-        password=os.getenv("CLICKHOUSE_PASSWORD", ""),
-        port=os.getenv("CLICKHOUSE_PORT", "8123"),
-    )
+    if psh_creds:
+        credentials = dict(
+            host=psh_creds["host"],
+            username=psh_creds["username"],
+            password=psh_creds["password"],
+            port=psh_creds["port"],
+        )
+    else:
+        credentials = dict(
+            host=os.getenv("CLICKHOUSE_HOST", "localhost"),
+            username=os.getenv("CLICKHOUSE_USERNAME", "default"),
+            password=os.getenv("CLICKHOUSE_PASSWORD", ""),
+            port=os.getenv("CLICKHOUSE_PORT", "8123"),
+        )
 
     client = clickhouse_connect.get_client(**credentials)
 
