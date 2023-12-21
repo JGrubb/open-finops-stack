d6b52f291837c775dd63a00bba3151942e31e412
John Grubb <git@grubb.johnnygrubb.com>
2023-12-21 10:43:30 -0500

    hardcode version

--- metabase/scripts/build.sh
+++ metabase/scripts/build.sh
@@ -4,13 +4,14 @@
 if [ -z ${PLATFORM_PROJECT_ENTROPY+x} ]; then 
     echo "Building Metabase locally"
     METABASE_HOME=$(pwd)/metabase
+    echo "Metabase home: $METABASE_HOME"
 # Platform.sh.
 else
     echo "Building Metabase on Platform.sh"
     METABASE_HOME=${PLATFORM_APP_DIR}/metabase
 fi
 
-    METABASE_VERSION=$(cat metabase.version)
+    METABASE_VERSION="0.47.10"
 
 # Download Metabase.
 echo "Downloading Metabase ($METABASE_VERSION)"
