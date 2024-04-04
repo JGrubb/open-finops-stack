d5816998abdd08073ea9496bb7c5112c7aa18ee2
John Grubb <git@grubb.johnnygrubb.com>
2024-04-04 14:57:03 -0400

    Type load_file in clickhouse

--- open_finops/clickhouse/__init__.py
+++ open_finops/clickhouse/__init__.py
@@ -1,60 +1,12 @@
-import time
-import pytz
+import sys
 
 from clickhouse.clickhouse_client import create_client
 from clickhouse_connect.driver.tools import insert_file
 
-
-def do_we_load_it(manifest: dict, **kwargs):
-    """
-    Determines whether to load a manifest based on the provided start and end dates.
-
-    Args:
-        manifest (dict): The manifest to be evaluated.
-        **kwargs: Additional keyword arguments.
-            - start_date (datetime): The start date for loading manifests.
-            - end_date (datetime): The end date for loading manifests.
-            - cur_version (str): The current version.
-
-    Returns:
-        bool: True if the manifest should be loaded, False otherwise.
-    """
-
-    if kwargs.get("start_date") and manifest["billing_period"] < kwargs[
-        "start_date"
-    ].replace(tzinfo=pytz.UTC):
-        print(
-            f"Skipping {manifest['billing_period']}: before configured start date of {kwargs['start_date']}"
-        )
-        return False
-
-    # If the manifest represents a billing period from after the configured end date, skip it
-    if kwargs.get("end_date") and manifest["billing_period"] >= kwargs[
-        "end_date"
-    ].replace(tzinfo=pytz.UTC):
-        print(
-            f"Skipping {manifest['billing_period']}: after configured end date of {kwargs['end_date']}"
-        )
-        return False
-
-    # If the manifest represents a billing period that has already been loaded, skip it
-    client = create_client()
-
-    result = client.command(
-        f"""
-        SELECT 1 FROM aws_state_{kwargs.get("cur_version")} 
-          WHERE execution_id = '{manifest['execution_id']}' 
-          AND billing_month = toDateTime('{manifest['billing_period'].strftime("%Y-%m-%d %H:%M:%S")}')"""
-    )
-    if result == 1:
-        print(
-            f"Skipping manifest {manifest['execution_id']} for {manifest['billing_period']} - already loaded"
-        )
-        return False
-    return True
+from open_finops import ManifestObject
 
 
-def load_file(manifest: dict, file_path: str):
+def load_file(manifest: ManifestObject, file_path: str):
     """
     Loads a file into ClickHouse database.
 
@@ -80,7 +32,7 @@ def load_file(manifest: dict, file_path: str):
                 client=client,
                 table=f"{manifest.vendor}_data_{manifest.version}",
                 file_path=file_path,
-                column_names=[column["name"] for column in manifest.columns],
+                column_names=[column.name for column in manifest.columns],
                 settings=settings,
             )
         elif file_path.endswith(".parquet"):
@@ -98,4 +50,4 @@ def load_file(manifest: dict, file_path: str):
             )
     except Exception as e:
         print(e)  # not this is not pretty and should be improved
-        raise
+        raise SystemExit
