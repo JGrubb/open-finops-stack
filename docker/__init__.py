94f3fa787832107e4b6c068054f40a57fa1777d8
John Grubb <git@grubb.johnnygrubb.com>
2025-06-19 17:49:42 -0400

    Add Docker package setup and metadata
    - Create docker/setup.py with core dependency
    - Add version and description metadata to docker/__init__.py
    - Define open-finops-docker as standalone installable package
    - Provides container configurations and deployment scripts
    
    This enables individual installation of Docker components:
    pip install -e ./docker/

---
+++ docker/__init__.py
@@ -0,0 +1,10 @@
+"""Open FinOps Stack Docker Package.
+
+This package provides Docker and container configurations:
+- Docker Compose configurations
+- Metabase integration with DuckDB
+- Container deployment scripts
+"""
+
+__version__ = "0.3.0"
+__description__ = "Docker configurations for Open FinOps Stack"
