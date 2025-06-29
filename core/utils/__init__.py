15fc1c0365233f6d0565e5e9a72e049706651267
John Grubb <git@grubb.johnnygrubb.com>
2025-06-28 23:00:49 -0400

    fix: Add missing table utilities to utils module imports

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -2,4 +2,7 @@
 
 from .s3 import S3Utils
 
-__all__ = ['S3Utils']
+# Import table utilities from the main utils module
+from ..utils import sanitize_table_name, create_table_name
+
+__all__ = ['S3Utils', 'sanitize_table_name', 'create_table_name']
