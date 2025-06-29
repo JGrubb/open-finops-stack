384c7fa258e75743b9083c6ed113a252b3353c68
John Grubb <git@grubb.johnnygrubb.com>
2025-06-28 22:43:14 -0400

    refactor: Remove old src/ directory structure
    Clean up deprecated src/ directory files that have been migrated
    to the new modular architecture under core/ and vendors/.

--- src/cli/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Open FinOps Stack CLI module."""
-
-from .main import main
-
-__all__ = ["main"]
--- src/pipelines/aws/__init__.py
+++
@@ -1,6 +0,0 @@
-"""AWS CUR pipeline module."""
-
-from .pipeline import run_aws_pipeline, aws_cur_source
-from .manifest import ManifestLocator, ManifestFile
-
-__all__ = ["run_aws_pipeline", "aws_cur_source", "ManifestLocator", "ManifestFile"]
